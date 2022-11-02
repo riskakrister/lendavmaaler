482eede6549edc40b3bce2d1de587ebb3b6d7366
Kaspar J. Pärtel <kasparjohannes@gmail.com>
2022-11-02 22:36:25 +0200

    adding typescript

--- src/pages/NavFullscreenMenu.js
+++
@@ -1,7 +0,0 @@
-import styles from "./NavFullscreenMenu.module.css";
-
-const NavFullscreenMenu = () => {
-  return <div className={styles.container__fullScreenMenu}></div>;
-};
-
-export default NavFullscreenMenu;
